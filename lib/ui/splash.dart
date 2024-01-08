4767831ceb260506697bb64c08a940ab87b078a0
kspsusmitha <kspsusmitha40@gmail.com>
2024-01-08 23:14:20 +0530

    08/01/2024

---
+++ lib/ui/splash.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset('assets/splashwallpaper1.jpg'),
+          Positioned(
+            right: 25,
+            left: 25,
+            top: 150,
+            child: Column(
+            children: [
+              Text("Good Food",
+              style: TextStyle(
+                fontSize: 46,
+                fontWeight: FontWeight.bold,
+               color: Colors.black54
+              ),
+              ),
+              Text("Deliciously Simple",
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.black54
+              ),
+              )
+            ],
+          )),
+          Positioned(
+            left: 50,
+            right: 50,
+            bottom: 150,
+            child: ElevatedButton(onPressed: (){}, child: Text('Start Cooking'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue.shade200.withOpacity(0.5),
+               shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+            ),
+            ))
+        ],
+      ),
+    );
+  }
+}
